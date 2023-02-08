904c3217b380a456dd4cf663b135f5a1c4c51c51
hsuifang <whalekiller03@gmail.com>
2023-02-09 07:33:54 +0800

    feat: add select in searchParams

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 function SearchParams() {
   const [location, setLocation] = useState("Seatle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breads = [];
+
   return (
     <div className="search-params">
       <form>
@@ -13,6 +18,41 @@ function SearchParams() {
           onChange={(e) => setLocation(e.target.value)}
           placeholder="location"
         />
+        <label htmlFor="animals">Animals</label>
+        <select
+          id="animals"
+          value={animal}
+          onChange={(e) => {
+            setAnimal(e.target.value);
+            setBreed("");
+          }}
+          onBlur={(e) => {
+            setAnimal(e.target.value);
+            setBreed("");
+          }}
+        >
+          <option />
+          {ANIMALS.map((animal) => (
+            <option value={animal} key={animal}>
+              {animal}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          disabled={breads.length === 0}
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+        >
+          <option />
+          {breads.map((breed) => (
+            <option value={breed} key={breed}>
+              {breed}
+            </option>
+          ))}
+        </select>
+
         <button>Submit</button>
       </form>
     </div>
