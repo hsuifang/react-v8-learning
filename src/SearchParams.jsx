bec3942fb1b2e2f9fb5d55c8f9c8474bce957151
hsuifang <whalekiller03@gmail.com>
2023-02-14 07:47:57 +0800

    chore: handle form event

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -27,7 +27,12 @@ function SearchParams() {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">Location</label>
         <input
           id="location"
